acd440906bf5d941a4b16334b7ee27a45dfe2549
mw-bl <bezerram533@gmail.com>
2024-11-05 14:37:08 -0300

    :tada: Adicionando model do itens cardapio

---
+++ MenuDigital/app/Models/ItensCardapio.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Itens_Cardapio extends Model
+{
+    use HasFactory;
+
+    protected $table = 'itens_cardapio';
+
+    protected $primaryKey = 'id_item_cardapio';
+
+    protected $fillable = [
+        'nome_produto',
+        'descricao',
+        'preco',
+        'link_imagem_itens',
+    ];
+
+    public function cardapios()
+    {
+        return $this->belongsToMany(Cardapio::class, 'pertence', 'fk_Itens_Cardapio_id_item_cardapio', 'fk_Cardapio_id_cardapio');
+    }
+}
