02f0e8670c765eac6ed4021fbc54cb2d13e84ada
mw-bl <bezerram533@gmail.com>
2024-11-05 14:35:52 -0300

    :tada: Adicionando model da empresa

---
+++ MenuDigital/app/Models/Empresa.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Empresa extends Model
+{
+    use HasFactory;
+
+    protected $table = 'empresa';
+
+    protected $primaryKey = 'id_empresa';
+
+    protected $fillable = [
+        'telefone',
+        'cnpj',
+        'nome',
+        'email',
+        'endereco',
+        'senha',
+    ];
+
+    public function cardapios()
+    {
+        return $this->hasMany(Cardapio::class, 'fk_Empresa_id_empresa');
+    }
+}
