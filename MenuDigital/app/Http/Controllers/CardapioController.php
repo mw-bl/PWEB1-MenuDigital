40975353944a72299d1857c6e1c2b91bb97dd489
Maria Eduarda Ferreira Batista <ferreirabatistam34@gmail.com>
2024-11-06 20:18:09 -0300

    adicionando controller de cardapio

--- MenuDigital/app/Http/Controllers/CardapioController.php
+++ MenuDigital/app/Http/Controllers/CardapioController.php
@@ -2,10 +2,64 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use App\Models\Cardapio;
 use Illuminate\Http\Request;
 
-class CardapioController
+class CardapioController extends Controller
 {
-    //
+    public function index()
+    {
+        $cardapios = Cardapio::all();
+        return view('cardapio.index', compact('cardapios'));
+    }
+
+    public function create()
+    {
+        return view('cardapio.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'descricao' => 'required|string|max:500',
+            'link_imagem' => 'required|url',
+            'fk_Empresa_id_empresa' => 'required|integer|exists:empresas,id_empresa'
+        ]);
+
+        Cardapio::create($request->all());
+        return redirect()->route('cardapio.index')->with('success', 'Cardápio criado com sucesso.');
+    }
+
+    public function show($id)
+    {
+        $cardapio = Cardapio::findOrFail($id);
+        return view('cardapio.show', compact('cardapio'));
+    }
+
+    public function edit($id)
+    {
+        $cardapio = Cardapio::findOrFail($id);
+        return view('cardapio.edit', compact('cardapio'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'descricao' => 'required|string|max:500',
+            'link_imagem' => 'required|url',
+            'fk_Empresa_id_empresa' => 'required|integer|exists:empresas,id_empresa'
+        ]);
+
+        $cardapio = Cardapio::findOrFail($id);
+        $cardapio->update($request->all());
+        return redirect()->route('cardapio.index')->with('success', 'Cardápio atualizado com sucesso.');
+    }
+
+    public function destroy($id)
+    {
+        $cardapio = Cardapio::findOrFail($id);
+        $cardapio->delete();
+        return redirect()->route('cardapio.index')->with('success', 'Cardápio deletado com sucesso.');
+    }
 }
+
