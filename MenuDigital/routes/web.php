274ee5cdaf4382d340eff3e7728ab66b9f73d1f3
Maria Eduarda Ferreira Batista <ferreirabatistam34@gmail.com>
2024-11-08 05:25:26 -0300

    consertando rotas que n~~ao estavam funcionando

--- MenuDigital/routes/web.php
+++ MenuDigital/routes/web.php
@@ -1,30 +1,25 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmpresaController;
 use App\Http\Controllers\CardapioController;
 use App\Http\Controllers\ItensCardapioController;
-use App\Http\Controllers\PesquisaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 // Rotas para Empresa
-Route::get('/empresa/cadastro', [EmpresaController::class, 'cadastro'])->name('empresa.cadastro');
-Route::post('/empresas', [EmpresaController::class, 'store'])->name('empresa.store');
-Route::get('/empresa/login', [EmpresaController::class, 'loginForm'])->name('empresa.loginForm');
-Route::post('/empresa/login', [EmpresaController::class, 'login'])->name('empresa.login');
+
+Route::get('/cadastro', [EmpresaController::class, 'cadastro'])->name('empresa.cadastro');
+Route::post('/cadastro', [EmpresaController::class, 'store'])->name('empresa.store');
+Route::get('/login', [EmpresaController::class, 'loginForm'])->name('empresa.loginForm');
+Route::post('/login', [EmpresaController::class, 'login'])->name('empresa.login');
+
 
 // Rotas para Cardapio
-Route::prefix('cardapio')->group(function () {
-    //Route::get('/', [CardapioController::class, 'index'])->name('cardapio.paginaPrincipal');
-    Route::get('/create', [CardapioController::class, 'create'])->name('cardapio.create');
-    Route::post('/', [CardapioController::class, 'store'])->name('cardapio.store');
-    Route::get('/{id}', [CardapioController::class, 'show'])->name('cardapio.show');
-    Route::get('/{id}/edit', [CardapioController::class, 'edit'])->name('cardapio.edit');
-    Route::put('/{id}', [CardapioController::class, 'update'])->name('cardapio.update');
-    Route::delete('/{id}', [CardapioController::class, 'destroy'])->name('cardapio.destroy');
+Route::middleware(['auth',])->group(function () {
+    Route::get('/empresa/cardapios', [CardapioController::class, 'manageAndCreate'])->name('cardapio.manageAndCreate');
+    Route::post('/empresa/cardapios', [CardapioController::class, 'store'])->name('cardapio.store');
 });
 
 // Rotas para ItensCardapio
